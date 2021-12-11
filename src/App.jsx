f519920eeb418102e2f79e6460f4ff0a253892fe
unknown <elaad2408@gmail.com>
2021-12-11 21:18:14 +0200

    temp arrange for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
+// components
+import TaskPoll from "./components/TaskPoll";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App ">
+      <TaskPoll header={"all tasks"} data={""} />
+      <TaskPoll header={"most urgent"} data={""} />
+      <div>
+        <button>add new one</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TaskPoolHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TaskPoolHeader = () => {
+  return (
+    <div className="TaskPoolHeaders">
+      <div>the task </div>
+      <div>urgency level</div>
+      <div>duration</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default TaskPoolHeader;
