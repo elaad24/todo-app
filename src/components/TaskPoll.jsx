3ea3ee1e94b4c3e0c23741586a8b11eb3dbdf4f5
unknown <elaad2408@gmail.com>
2021-12-11 21:19:15 +0200

    add detailes for task pool

--- src/components/TaskPoll.jsx
+++ src/components/TaskPoll.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Task from "./Task";
+import TaskPoolHeader from "./TaskPoolHeader";
 
-const TaskPoll = () => {
-  return <div className="border d-flex flex-column"></div>;
+const TaskPoll = ({ header, data }) => {
+  return (
+    <div className="taskPool ">
+      <h2 className="header">{header}</h2>
+      <TaskPoolHeader />
+      <Task />
+    </div>
+  );
 };
 
 export default TaskPoll;
