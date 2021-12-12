828c19130e972b020fae33fb4be6443c89b43192
unknown <elaad2408@gmail.com>
2021-12-12 21:38:45 +0200

    add acordion component for component

--- src/components/TaskPoll.jsx
+++ src/components/TaskPoll.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Task from "./Task";
 import TaskPoolHeader from "./TaskPoolHeader";
+import { Accordion } from "react-bootstrap";
 
 const TaskPoll = ({ header, data }) => {
   return (
     <div className="taskPool ">
       <h2 className="header">{header}</h2>
       <TaskPoolHeader />
-      <Task />
+      <Accordion flush>
+        <Accordion.Item eventKey="5">
+          <Accordion.Header>
+            <Task taskHeader={""} urgencyNumber={""} duration={""} />
+          </Accordion.Header>
+          <Accordion.Body>{`taskBody`}</Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 };
