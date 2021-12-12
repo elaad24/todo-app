cb9573f68cf6f76a2ccbf2c263568c0bf014ff8f
unknown <elaad2408@gmail.com>
2021-12-12 21:40:51 +0200

    add react-bootstrap and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 
 // components
 import TaskPoll from "./components/TaskPoll";
+import HalfTaskPoll from "./components/HalfTaskPoll";
 
 function App() {
   return (
     <div className="App ">
       <TaskPoll header={"all tasks"} data={""} />
-      <TaskPoll header={"most urgent"} data={""} />
+      <div className="double_stack ">
+        <HalfTaskPoll header={"most urgent"} data={""} />
+        <HalfTaskPoll header={"completed"} data={""} />
+      </div>
       <div>
         <button>add new one</button>
       </div>
