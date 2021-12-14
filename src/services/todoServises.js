7c809f23376103b21638ee4982f73841edfdec4a
unknown <elaad2408@gmail.com>
2021-12-14 17:47:08 +0200

    add conection and function from backend to front

--- src/services/todoServises.js
+++ src/services/todoServises.js
@@ -8,3 +8,9 @@ export function getAllToDos() {
 export function getUrgentToDos() {
   return http.get(`${apiUrl}/urgent`);
 }
+export function getCompletedToDos() {
+  return http.get(`${apiUrl}/completed`);
+}
+export function setTodoAsCompleted(todoID) {
+  return http.put(`${apiUrl}/accomplished?id=${todoID}`);
+}
