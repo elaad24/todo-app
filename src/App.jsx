c4799c2ff68ec4ac677cb6e06b072d6b54b12ab8
unknown <elaad2408@gmail.com>
2021-12-16 18:52:11 +0200

    implament the todo midal

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import "./App.css";
 // components
 import TaskPoll from "./components/TaskPoll";
 import HalfTaskPoll from "./components/HalfTaskPoll";
+import ItemModal from "./components/ItemModal";
+import { useState } from "react";
 
 function App() {
+  const [modalState, setModalState] = useState(false);
   return (
-    <div className="App ">
-      <TaskPoll header={"all tasks"} data={""} />
-      <div className="double_stack ">
-        <HalfTaskPoll header={"most urgent"} data={""} />
-        <HalfTaskPoll header={"completed"} data={""} />
+    <>
+      <div className="App ">
+        <TaskPoll header={"all tasks"} data={""} />
+        <div className="double_stack ">
+          <HalfTaskPoll header={"most urgent"} data={""} />
+          <HalfTaskPoll header={"completed"} data={""} />
+        </div>
+        <div>
+          <ItemModal setModalState={setModalState} />
+        </div>
       </div>
-      <div>
-        <button>add new one</button>
-      </div>
-    </div>
+    </>
   );
 }
 
